f4b07e5fdef39f9ecc927928aa5b4e63cb7fcc49
Rahano <wojciechmuras@wp.pl>
2020-10-20 14:38:19 +0200

    Add input parameter to reverse string

--- src/main/java/ppkwu/rest_rev/Controller.java
+++ src/main/java/ppkwu/rest_rev/Controller.java
@@ -1,13 +1,14 @@
 package ppkwu.rest_rev;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
     @RequestMapping(path = "/revertString")
-    public String reverseString(){
+    public String reverseString(@RequestParam(value = "input", required = false) String input){
 
         return null;
 
