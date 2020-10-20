d69e74543aecaa5ba33ca88058122f1252fbe286
Rahano <wojciechmuras@wp.pl>
2020-10-20 14:28:35 +0200

    Initialize project

---
+++ src/main/java/ppkwu/rest_rev/RestRevApplication.java
@@ -0,0 +1,13 @@
+package ppkwu.rest_rev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestRevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ppkwu/rest_rev/RestRevApplicationTests.java
@@ -0,0 +1,13 @@
+package ppkwu.rest_rev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
