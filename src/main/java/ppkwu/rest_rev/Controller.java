e296c7240d5921f234589ff4a896eb357759f07c
Rahano <wojciechmuras@wp.pl>
2020-10-20 14:34:05 +0200

    Add mapping for revert string

--- src/main/java/ppkwu/rest_rev/Controller.java
+++ src/main/java/ppkwu/rest_rev/Controller.java
@@ -1,8 +1,16 @@
 package ppkwu.rest_rev;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @RequestMapping(path = "/revertString")
+    public String reverseString(){
+
+        return null;
+
+    }
 
 }
